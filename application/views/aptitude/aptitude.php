0d31335fa565d4ad8350d4b7281c6a7bcabb2c55
Apoorva Dave <Apoorva Dave>
2017-03-11 15:07:50 +0530

    Added Categories to Aptitude Page

--- application/views/aptitude/aptitude.php
+++ application/views/aptitude/aptitude.php
@@ -9,22 +9,83 @@
 <body>
 	<?php include __DIR__."/../includes/header.php" ?>
 	<div class="container">
-		<div class="question-categories">
-			<h3>Aptitude Questions</h3>
-			<div class="ul">
-				<li><a href="#">General Aptitude</a></li>
-				<li><a href="#">Vocabulary</a></li>
-				<li><a href="#">Quantitative Aptitude</a></li>
+	<div class="panel panel-info">
+		<div class="panel-heading">
+	    	<h3 class="panel-title">Aptitude Questions</h3>
+	  	</div>
+  		<div class="panel-body">
+    		<div class="question-categories">
+			<h4>Quantitative Aptitude</h4>
+			<ul>
+				<div class="col-md-4">
+					<li><a href="#">Number System</a></li>
+					<li><a href="#">Speed and Distance</a></li>
+					<li><a href="#">Boats and Streams</a></li>
+				</div>
+				<div class="col-md-4">
+					<li><a href="#">Profit and Loss</a></li>
+					<li><a href="#">Averages and Mixtures</a></li>
+					<li><a href="#">Ratio and Proportion</a></li>
+				</div>
+				<div class="col-md-4">
+					<li><a href="#">Simple and Compund Interest</a></li>
+					<li><a href="#">Height and Distance</a></li>
+					<li><a href="#">Probability</a></li>
+				</div>
+			</ul>
+			
+			<h4>Verbal Ability</h4>
+			<ul>
+				<div class="col-md-4">
+					<li><a href="#">Grammar</li>
+					<li><a href="#">Synonyms and Antonyms</a></li>
+				</div>
+				<div class="col-md-4">
+					<li><a href="#">Error Correction</a></li>
+					<li><a href="#">Idioms and Phrases</a></li>
+				</div>
+				<div class="col-md-4">
+					<li><a href="#">Comprehension</a></li>
+					<li><a href="#">Fill in the blanks</a></li>
+				</div>
+			</ul>
+
+			<h4>Logical Reasoning</h4>
+			<ul>
+				<div class="col-md-4">
+					<li><a href="#">Cubes and Dices</li>
+					<li><a href="#">Puzzles</a></li>
+				</div>
+				<div class="col-md-4">
+					<li><a href="#">Direction and Senses</a></li>
+					<li><a href="#">Statements and Assumptions</a></li>
+				</div>
+				<div class="col-md-4">
+					<li><a href="#">Logical Deductions</a></li>
+					<li><a href="#">Blood Relations</a></li>
+				</div>
+			</ul>
+
+		</div>
+	</div>
+	</div>
+	<div class="panel panel-info">
+		<div class="panel-heading">
+	    	<h3 class="panel-title">Coding Questions</h3>
+	  	</div>
+  		<div class="panel-body">
+			<div class="question-categories">
+				<ul>
+					<li><a href="#">Arrays</a></li>
+					<li><a href="#">Linked Lists</a></li>
+					<li><a href="#">Strings</a></li>
+					<li><a href="#">Trees</a></li>
+					<li><a href="#">Stacks and Queues</a></li>
+					<li><a href="#">Miscellaneous</a></li>
+				</ul>
 			</div>
 		</div>
-		<div class="question-categories">
-			<h3>Coding Questions</h3>
-			<div class="ul">
-				<li><a href="#">Hashing</a></li>
-				<li><a href="#">Stacks</a></li>
-				<li><a href="#">Queues</a></li>
-			</div>
-		</div>
+	</div>	
 	</div>
 	<?php include __DIR__."/../includes/footer.php" ?>
 </body>
