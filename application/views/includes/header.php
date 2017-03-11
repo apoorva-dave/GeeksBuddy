bc8c01ec69d068aba3c28c1eafc137aa492a2123
anamritraj <anandamritraj904@gmail.com>
2017-03-11 13:29:38 +0530

    Modify header to include topicwise and company wise

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,23 +1,41 @@
 <nav class="navbar navbar-default">
 	<div class="container">
 		<div class="navbar-header">
-		      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-		        <span class="sr-only">Toggle navigation</span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		        <span class="icon-bar"></span>
-		      </button>
-		    <h1>GeeksBuddy</h1>  
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">GeeksBuddy</a>
 	</div>
   
     <!-- Brand and toggle get grouped for better mobile display -->
    	    <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    
+      
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="<?php echo base_url() ?>index.php/Home">Home</a></li>
-        <li><a href="#">About Us</a></li>
-        <li><a href="<?php echo base_url() ?>index.php/Categories">Category</a></li>
+        <li>
+          <div class="dropdown">
+            <button class="dropbtn">Company Wise</button>
+            <div class="dropdown-content">
+            <a href="#">UHG</a>
+            <a href="#">Oracle</a>
+            <a href="#">CA</a>
+            <a href="#">Mu Sigma</a>
+            <a href="#">Capegemini</a>
+            </div>
+          </div>
+        </li>
+        <li>
+          <div class="dropdown">
+            <button class="dropbtn">Topic Wise</button>
+            <div class="dropdown-content">
+            <a href="<?php echo base_url()?>index.php/Aptitude">Aptitude</a>
+            <a href="<?php echo base_url()?>index.php/Coding">Coding</a>
+            </div>
+          </div>
+        </li>
         <li><a href="<?php echo base_url() ?>index.php/Login">Login</a></li>
         <li><a href="#">Register</a></li>
 
