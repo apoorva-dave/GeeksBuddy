9b05f9fb7a5926f6ab8deb02959a6c7f7bdcf3fb
Apoorva Dave <Apoorva Dave>
2017-03-16 01:32:55 +0530

    Fix typo.

--- application/models/Model_questions.php
+++ application/models/Model_questions.php
@@ -33,15 +33,32 @@ class Model_questions extends CI_Model
 	}
 
 	// Get questions based on a company-slug
-	public function getQuestionsbyCompany($company_slug)
+	public function getQuestionsbyCompany($company_slug,$page = 1,$per_page)
 	{
 		if (!$company_slug) {
 			return null;		
 		}
 		$this->db->select('*');
 		$this->db->where('company_slug',$company_slug);
-		$query=$this->db->get('objective_questions');
-		return $query->result();
+		$this->db->limit($per_page, ($page - 1) * $per_page);
+		$query_1 = $this->db->get('objective_questions');
+		// Apart from rsults, we need ki kitne no of pages hain toal! 
+		//qki apanko wahan dikhana hai!
+		
+		$this->db->where('company_slug',$company_slug);
+		$query_2 = $this->db->count_all_results('objective_questions');
+		$data['questions'] =  $query_1->result();
+		$data['no_of_pages'] =  $query_2/ $per_page; // toal rows / no_of_ques per page!
+		return $data;
 	}
-
+	public function getCompanyName($company_slug)
+	{
+		if (!$company_slug) {
+			return null;		
+		}
+		$this->db->select('name');
+		$this->db->where('company_slug',$company_slug);
+		$query=$this->db->get('companies');
+		return $query->result()[0]->name;	
+	}
 }
