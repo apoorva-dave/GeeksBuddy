925d3786afadcb8a7b002af63aea296731e342bf
Apoorva Dave <Apoorva Dave>
2017-03-15 19:14:40 +0530

    Fix project root path.

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/online-quiz/';
+$config['base_url'] = 'http://localhost/online-exam/';
 
 /*
 |--------------------------------------------------------------------------
