3f632fc99d494ff14282e1d886f0af211bc923e9
anamritraj <anandamritraj904@gmail.com>
2017-03-19 19:08:59 +0530

    Test returned and saved in database.

--- application/models/Model_test.php
+++ application/models/Model_test.php
@@ -20,21 +20,35 @@ class Model_test extends CI_Model
 		/////////////////////////////////////////
 		// TODO Save this test in the database //
 		/////////////////////////////////////////
-		
-		// get user_id from session
-		
+		// get user_email from session
+		$test_data['user_email'] = $this->session->userdata('is_logged_in');
+
 		// create comma seperated values of question ids
-		
-		//create comma seperated values of answers
-		
-		// make answers 0
-		
+		$test_data['question_ids'] = [];
+		$test_data['right_options'] = [];
+		foreach ($data['questions'] as $ques) {
+			array_push($test_data['question_ids'], $ques->id);
+			array_push($test_data['right_options'], $ques->right_option);
+		}
+
 		// time alloted = $time 
+		$test_data['time_alloted'] = $time;
 		// time time used = 0
-		
+		$test_data['time_used'] = 0;
 		// comma sperated values of question_ids which are answered
+		$test_data['answered_questions'] = "";
 		// comma sperated values of answers which are answered
-		   
+		$test_data['answered_answers'] = "";
+
+		$test_data['question_ids'] = implode(",",$test_data['question_ids']);
+		$test_data['right_options'] = implode(",",$test_data['right_options']);
+
+		$test_data['updated_at'] = date('Y-m-d h-m-s');
+		// echo "<pre>";
+		// var_dump($test_data);
+		// die();
+		$query = $this->db->insert('tests', $test_data);
+
 		return $data;
 	}
 
